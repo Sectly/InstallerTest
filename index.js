9226ee48741b9ab3f7932b57413df0b05a807516
Sectly <67114413+Sectly@users.noreply.github.com>
2022-10-21 14:05:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const fs = require("fs");
+const logcon = require("logcon");
+const axios = require("axios");
 
+let downloadeddata = "";
+
+function scrape(res) {
+  downloadeddata = res.body
+
+  fs.writeFile('2pac.txt', lyrics, (err) => {
+    // throws an error, you could also catch it here
+    if (err) throw err;
+
+    // success case, the file was saved
+    console.log('Lyric saved!');
+  });
+}
+
+axios.get('https://jsonplaceholder.typicode.com/posts/1').then(res => scrape(res)).catch(err => logcon.error(err))
